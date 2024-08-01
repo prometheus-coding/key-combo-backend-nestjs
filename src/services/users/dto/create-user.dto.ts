8cf202d0c6568622c0c3647d9b64a0e236e0501b
Ethan <ethanbone435@gmail.com>
2024-08-01 21:41:09 +0200

    fixed dto for front end data

--- src/services/users/dto/create-user.dto.ts
+++ src/services/users/dto/create-user.dto.ts
@@ -1,9 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'John Doe', description: 'The name of the user' })
-  name: string;
+  @ApiProperty({ example: 'xXjohndoeXx_swaggerITA', description: 'The username of the user' })
+
+  username: string;
+  @ApiProperty({ example: 'John', description: 'The name of the user' })
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'The second name of the user' })
+  lastName: string;
 
   @ApiProperty({ example: 'john@example.com', description: 'The email of the user' })
   email: string;
+
+  @ApiProperty({ example: 'Password123!', description: 'The password of the user' })
+  password: string;
+
+  @ApiProperty({ example: 60, description: 'The combo duration in seconds', default: 0 })
+  combo_duration_in_seconds: number;
+
+  @ApiProperty({ example: 1000, description: 'The total number of keys pressed', default: 0 })
+  total_key_pressed: number;
 }
