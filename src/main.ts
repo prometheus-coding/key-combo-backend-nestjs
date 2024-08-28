e41f850cf503e2c6490978146d747f963830800d
MindfulLearner <dimajosh1009@gmail.com>
2024-08-28 22:28:13 +0200

    test e debug con local 3001, aggiunti cors abilitati

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,21 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+// cors
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors(); // abilitato cors per ricevere posto da front
+
+  // cors abilitato
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true
+  };
+  app.enableCors(corsOptions); // abilitato cors per ricevere posto da front
+
   app.setGlobalPrefix('api/v1');
   const config = new DocumentBuilder()
     .setTitle('K-Combo API')
