bec6ece2e702e1dcacaa4e35786300086a4b9559
MindfulLearner <dimajosh1009@gmail.com>
2024-08-29 13:22:56 +0200

    swag tolto argomenti da cors ora funzionante

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,20 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 // cors
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // cors abilitato
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true
-  };
-  app.enableCors(corsOptions); // abilitato cors per ricevere posto da front
-
+  /* //  cors abilitato
+          const corsOptions: CorsOptions = {
+            origin: 'http://localhost:3000/',
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+            allowedHeaders: 'Content-Type, Accept',
+            credentials: true
+          };
+          app.enableCors(); // abilitato cors per ricevere posto da front
+        */
   app.setGlobalPrefix('api/v1');
   const config = new DocumentBuilder()
     .setTitle('K-Combo API')
